a367fc05630eeab9f93d099adc67620484d5d191
Acasez <edvin.sanne@gmail.com>
2026-05-07 15:33:05 +0200

    Added airplane, boat and motorcycles classes

---
+++ Motorcycle.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_Garage_Task
+{
+    internal class Motorcycle(string name, string registerID, string color) : Vehicle(name, registerID, color)
+    {
+
+    }
+}
